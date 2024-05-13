61732174800b1fd3e97ee2ae165abd781b880b69
dymitr <woozywonder7@gmail.com>
2024-05-13 11:37:54 +0200

    Add default layout.

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="mx-auto max-w-[2000px]">
+    <div class="w-full pt-[70px]">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>NIMBUS!</div>
+  <div class="v-container">
+    <div class="h-[300px] w-full bg-red-200"></div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
