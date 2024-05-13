8e0d9b5cce85124da45a06cac3158d95cfcfe135
dymitr <woozywonder7@gmail.com>
2024-05-13 12:05:37 +0200

    Add google fonts module.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,16 @@
 export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss',
-    '@nuxt/image'
+    '@nuxt/image',
+    '@nuxtjs/google-fonts'
   ],
   css: ['~/assets/css/main.css'],
   devServer: {
     port: 7777,
   },
+  googleFonts: {
+    families:{
+      Poppins: [300, 400, 500, 600, 700]
+    }
+  }
 })
