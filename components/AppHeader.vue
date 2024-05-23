ea7c59993d15b4ba773d1f9bd6ea4ef77e903850
dymitr <woozywonder7@gmail.com>
2024-05-23 12:01:42 +0200

    Implement header mobile markup. Update icons.

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -3,32 +3,32 @@
     class="v-container fixed z-20 w-full border-b-[1px] border-gray-300 bg-white py-0 transition-all duration-300 ease-in-out 2xl:w-[1600px]"
     :class="[scrolled && '-translate-y-24']"
   >
-    <div class="flex h-full w-full justify-between bg-white py-5 font-medium">
-      <div class="flex items-center gap-8">
+    <div class="flex h-full w-full justify-between bg-white py-4">
+      <div class="flex items-center gap-10">
         <NuxtLink to="/">
-          <NuxtImg src="/icons/nimbus.svg" width="115" />
+          <NuxtImg src="/icons/nimbus.svg" width="105" />
         </NuxtLink>
 
-        <div
-          v-for="item in links"
-          :key="item"
-          class="cursor-pointer"
-          @click="$router.push({ hash: `#${item.toLowerCase()}` })"
-        >
-          {{ item }}
+        <div class="hidden gap-6 lg:flex">
+          <div
+            v-for="item in links"
+            :key="item"
+            class="cursor-pointer"
+            @click="$router.push({ hash: `#${item.toLowerCase()}` })"
+          >
+            {{ item }}
+          </div>
         </div>
       </div>
 
-      <div class="flex gap-2">
-        <div>
+      <div class="flex gap-4">
+        <div class="hidden gap-2 md:flex">
           <div
             class="flex w-full items-center justify-center gap-2 rounded-2xl border-[1px] border-primary px-5 py-3 text-sm text-primary sm:w-max"
           >
             <span> Get a demo </span>
           </div>
-        </div>
 
-        <div>
           <div
             class="flex w-full items-center justify-center gap-2 rounded-2xl bg-primary px-5 py-3 text-sm text-white sm:w-max"
           >
@@ -36,6 +36,8 @@
             <NuxtImg src="/icons/ai-white.svg" class="min-w-4" />
           </div>
         </div>
+
+        <NuxtImg src="/icons/burger-menu.svg" class="block min-w-8 lg:hidden" />
       </div>
     </div>
   </div>
@@ -50,7 +52,7 @@ const { y } = useWindowScroll()
 watch(
   () => y.value,
   (newV, oldV) => {
-    if (newV > 20 && newV > oldV) {
+    if (newV > 70 && newV > oldV) {
       scrolled.value = true
     } else if (newV < oldV) {
       scrolled.value = false
