8d87e6be82b2f92a433418b29c36cf697a4874b3
dymitr <woozywonder7@gmail.com>
2024-05-25 08:08:25 +0200

    Add modal form email validation.

--- components/base/TrialForm.vue
+++ components/base/TrialForm.vue
@@ -5,18 +5,75 @@
     </h2>
 
     <p class="text-sm font-light text-gray-600 md:text-base">
-      Sign up now to be among the first to experience Nimbus, the cutting-edge
-      AI platform for data processing. Gain exclusive early access and enjoy
-      special benefits tailored just for our early adopters.
+      Sign up now for early access to Nimbus, the cutting-edge AI data
+      processing platform. Enjoy exclusive benefits as one of our first users!
     </p>
 
-    <input
-      type="text"
-      class="max-h-[54px] w-full rounded-2xl border-[1px] border-gray-300 px-4 py-4 outline-none"
-    />
+    <form id="trialForm" @submit.prevent="validate">
+      <label
+        for="emailInput"
+        class="ml-1 text-sm text-gray-500 transition-colors duration-500"
+        :class="[error && 'text-red-500']"
+        >Email</label
+      >
+      <input
+        v-model="email"
+        ref="inputRef"
+        id="emailInput"
+        type="text"
+        class="max-h-[54px] w-full rounded-2xl border-[1px] border-gray-300 px-4 py-4 outline-none transition-colors duration-500"
+        :class="[error && 'border-red-500']"
+      />
+      <div class="h-5">
+        <Transition name="fade">
+          <label
+            v-if="error"
+            for="emailInput"
+            class="ml-1 text-sm text-red-500"
+          >
+            Please provide a valid address
+          </label>
+        </Transition>
+      </div>
+    </form>
 
-    <BaseButton title="Submit" class="tracking-widest" />
+    <BaseButton
+      title="Submit"
+      class="tracking-widest"
+      type="submit"
+      form="trialForm"
+    />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const email = ref('')
+const inputRef = ref(null)
+const error = ref(false)
+const success = ref(false)
+
+function validate() {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  error.value = !emailRegex.test(email.value)
+
+  if (!error.value) success.value = true
+}
+
+onMounted(() => {
+  if (inputRef.value) {
+    ;(inputRef.value as HTMLElement).focus()
+  }
+})
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 500ms ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
