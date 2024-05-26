5aa3acf6f5c9afdada8e26c8e43c79e6621f8f90
dymitr <woozywonder7@gmail.com>
2024-05-26 12:32:41 +0200

    Add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,14 @@
     <div class="w-full">
       <AppHeader />
       <slot />
+      <div class="v-container py-0">
+        <div class="border-t-[1px] border-gray-300 py-6 text-sm text-[#838b7f]">
+          <div class="flex items-center justify-end gap-2">
+            <NuxtImg src="/icons/copyright.svg" class="w-3" />
+            <span>2024 NIMBUS • All rights reserved</span>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
